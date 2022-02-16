ad791cc29a8f5ec6bfcdba6a6c578a6843ad7e72
LWG04 <lukawilsongreen@gmail.com>
2022-02-16 18:57:58 +0000

    commit

--- main.py
+++ main.py
@@ -1,7 +1,3 @@
 if __name__ == "__main__":
-    if input("Do you want the working version? (Y/n) ").lower() == "n":
-        import python_2048
-        python_2048.run()
-    else:
-        import python_2048_working
-        python_2048_working.run()
+    import python_2048
+    python_2048.run()
